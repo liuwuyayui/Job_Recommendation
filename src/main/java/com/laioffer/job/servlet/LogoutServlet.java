d6c1c1b8ed195e9ee87c7dea738abea2f1b0f910
liuwuyayui <liuwuyayui@gmail.com>
2021-02-20 11:52:49 -0800

    after deployment

--- src/main/java/com/laioffer/job/servlet/LogoutServlet.java
+++ src/main/java/com/laioffer/job/servlet/LogoutServlet.java
@@ -12,19 +12,10 @@ import java.io.IOException;
 public class LogoutServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-  
-  }
-  
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     HttpSession session = request.getSession(false);
     if (session != null) {
       session.invalidate();
     }
-  
-    ObjectMapper mapper = new ObjectMapper();
-    response.setContentType("application/json");
-    ResultResponse resultResponse = new ResultResponse("OK");
-    mapper.writeValue(response.getWriter(), resultResponse);
+    response.sendRedirect("index.html");
   }
 }
